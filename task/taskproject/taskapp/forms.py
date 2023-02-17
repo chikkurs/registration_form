1d43da7719f33fa23dfe11e904031347167d969c
chikkurs <54857913+chikkurs@users.noreply.github.com>
2023-02-17 14:46:31 +0530

    Delete task directory

--- task/taskproject/taskapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Data
-class Data_form(forms.ModelForm):
-    class Meta:
-        model=Data
-        fields=['name','date_of_birth','age','gender','phone','mail','address','department','purposes']
-
-        
